a3d006b0042130694fadaaa247b9854dafcb16a1
JMmmmuu <mkyseok1550@gmail.com>
2019-03-11 19:28:47 +0900

    add help & wrong cmd

---
+++ Projects/1_sicShell/codes/shell.c
@@ -0,0 +1,17 @@
+/*************************************************
+ *************************************************
+ ******************** Yuseok *********************
+ ******************** 190000 *********************
+ *************************************************/
+#include "header.h"
+
+void help() {
+    printf("h[elp]\nd[ir]\nq[uit]\nhi[story]\n");
+    printf("du[mp] [start, end]\n");
+    printf("e[dit] address, value\n");
+    printf("f[ill] start, end, value\n");
+    printf("reset\n");
+    printf("opcode mnemonic\nopcodelist\n");
+}
+
+
