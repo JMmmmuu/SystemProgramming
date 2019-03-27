00831656e34460e7464dd531161cb7756ace0f65
JMmmmuu <mkyseok1550@gmail.com>
2019-03-28 07:23:16 +0900

    add test asm file

---
+++ Projects/2_SIC_Assembler/codes/test_1.asm
@@ -0,0 +1,4 @@
+. This is comment line
+. For the test
+. Work well?
+. I don't know~~
---
+++ Projects/2_SIC_Assembler/codes/test_2.asm
@@ -0,0 +1,6 @@
+. This is comment line
+. For the test
+. Work well?
+. I don't know~~
+NAME    START   1000
+        END     NAME
